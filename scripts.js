15b2f62baaf671c8d39bbfcec65625a0114bafbb
Potato-Man5 <potatoman5520@gmail.com>
2024-09-15 23:37:27 +0800

    javascript addition

---
+++ scripts.js
@@ -0,0 +1,10 @@
+var w = 1;
+var x = 1;
+var y = w + x;
+var z = w - x;
+function addOne() {
+    document.getElementById("shopbutton").innerHTML="" + y ;
+}
+function minusOne() {
+    document.getElementById("shopbutton").innerHTML="" + z ;
+}
